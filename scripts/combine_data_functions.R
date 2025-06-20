a6d28cdcdbbda0d2d061cfed50a28e0713c25cff
Jarrett Byrnes <jarrett.byrnes@umb.edu>
2025-06-20 10:01:07 -0400

    Cleanup innacurate standardization

--- scripts/combine_data_functions.R
+++ scripts/combine_data_functions.R
@@ -2,6 +2,7 @@ source("scripts/data_checking_functions.R")
 library(lubridate)
 
 mean_na <- function(x) mean(x, na.rm = TRUE)
+max_na <- function(x) max(x, na.rm = TRUE)
 
 process_one_study_to_combine <- function(datasetname, path = "data/clean_data/timeseries/"){
   print(glue("Working on {datasetname}"))
@@ -37,9 +38,9 @@ process_one_study_to_combine <- function(datasetname, path = "data/clean_data/ti
       Longitude = mean_na(Longitude),
       Sample_Unit_Size_sq_m = mean_na(Sample_Unit_Size_sq_m),
       kelpPresent = max(kelpPresent),
-      max_Depth = max(Depth_m),
+      max_Depth = max_na(Depth_m),
       mean_Depth = mean_na(Depth_m),
-      min_Depth = min(Depth_m),
+      min_Depth = min(Depth_m, na.rm = TRUE),
       sasDate = mean_na(sasDate),
       Biomass_kg_wet_per_sq_m = mean_na(Biomass_kg_wet_per_sq_m),
       Stipe_Density_num_per_sq_m = mean_na(Stipe_Density_num_per_sq_m),
@@ -67,7 +68,8 @@ process_one_study_to_combine <- function(datasetname, path = "data/clean_data/ti
     ) |>
     
     # aggregate measurements by period - so getting average kelp cover
-    group_by(Study, Site, period) |>
+    # per site per period - 1 measurement
+   group_by(Study, Site, period) |>
     summarize(
       Latitude = mean_na(Latitude),
       Longitude = mean_na(Longitude),
@@ -76,25 +78,26 @@ process_one_study_to_combine <- function(datasetname, path = "data/clean_data/ti
       max_Depth = max(max_Depth),
       mean_Depth = mean_na(mean_Depth),
       min_Depth = min(min_Depth),
-      biomass_KG_wet_per_sq_m = mean_na(Biomass_kg_wet_per_sq_m),
-      Stipe_Density_num_per_sq_m = mean_na(Stipe_Density_num_per_sq_m),
-      individual_per_sq_m = mean_na(Individual_Density_num_per_sq_m),
+      biomass_kg_wet_per_sq_m = mean_na(Biomass_kg_wet_per_sq_m),
+      stipe_density_num_per_sq_m = mean_na(Stipe_Density_num_per_sq_m),
+      individual_density_num_per_sq_m = mean_na(Individual_Density_num_per_sq_m),
       percent_cover = mean_na(Percent_Cover),
-      biomass_KG_wet_per_sq_m_STD = sd(Biomass_kg_wet_per_sq_m),
-      Stipe_Density_num_per_sq_m_STD = sd(Stipe_Density_num_per_sq_m),
-      individual_per_sq_m_STD = sd(Individual_Density_num_per_sq_m),
-      percent_cover_STD = sd(Percent_Cover),
+      # not sure why this was added - remove
+    # biomass_KG_wet_per_sq_m_STD = sd(Biomass_kg_wet_per_sq_m),
+    # Stipe_Density_num_per_sq_m_STD = sd(Stipe_Density_num_per_sq_m),
+    # individual_per_sq_m_STD = sd(Individual_Density_num_per_sq_m),
+    # percent_cover_STD = sd(Percent_Cover),
       .groups = "drop"
     ) |>
-    group_by(Study) |>
-    mutate(  has_bm = sum(!is.na(biomass_KG_wet_per_sq_m)),
-             has_sd = sum(!is.na(Stipe_Density_num_per_sq_m)),
-             has_id = sum(!is.na(individual_per_sq_m)),
+    rename_with(tolower) |>
+    group_by(study) |>
+    mutate(  has_bm = sum(!is.na(biomass_kg_wet_per_sq_m)),
+             has_sd = sum(!is.na(stipe_density_num_per_sq_m)),
+             has_id = sum(!is.na(individual_density_num_per_sq_m)),
              has_pc = sum(!is.na(percent_cover)),
              use_which = which.max(c(has_bm[1], has_sd[1], has_id[1], has_pc[1]))
           ) |>
     ungroup() |>
-    rename_with(tolower) |>
     
     
     # add focal kelp with preference order biomass, stipes, individuals, percent
@@ -102,17 +105,17 @@ process_one_study_to_combine <- function(datasetname, path = "data/clean_data/ti
       focalKelp = case_when(
         use_which==1  ~ biomass_kg_wet_per_sq_m,
         use_which==2  ~ stipe_density_num_per_sq_m,
-        use_which==3  ~ individual_per_sq_m,
+        use_which==3  ~ individual_density_num_per_sq_m,
         use_which==4  ~ percent_cover,
         TRUE ~ NA_real_
       ),
-      focalKelp_STD = case_when(
-        use_which==1  ~ biomass_kg_wet_per_sq_m_std,
-        use_which==2  ~ stipe_density_num_per_sq_m_std,
-        use_which==3  ~ individual_per_sq_m_std,
-        use_which==4  ~ percent_cover_std,
-        TRUE ~ NA_real_
-      ),
+      # focalKelp_STD = case_when(
+      #   use_which==1  ~ biomass_kg_wet_per_sq_m_std,
+      #   use_which==2  ~ stipe_density_num_per_sq_m_std,
+      #   use_which==3  ~ individual_density_num_per_sq_m_std,
+      #   use_which==4  ~ percent_cover_std,
+      #   TRUE ~ NA_real_
+      # ),
       focalUnit = case_when(
         use_which==1  ~ "biomass",
         use_which==2  ~ "stipe_density",
