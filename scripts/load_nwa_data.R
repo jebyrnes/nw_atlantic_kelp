5a02c234d307077d12722c26827ae43a2c43ee4a
Jarrett Byrnes <jarrett.byrnes@umb.edu>
2025-06-24 18:40:00 -0400

    New data

---
+++ scripts/load_nwa_data.R
@@ -0,0 +1,20 @@
+library(readr)
+library(dplyr)
+
+nwa_dat <- read_csv("data/nwa_with_env.csv") |>
+  # Rescaling
+  mutate(rescaled_std_by_ecoregion = scales::rescale(focal_std_by_ecoregion, c(0,1)),
+         year_z = (year - mean(year))/sd(year)) |>
+  # for plotting, get N-S
+  mutate(ecoregion = factor(ecoregion, levels = 
+                              c("Virginian",
+                                "Gulf of Maine/Bay of Fundy",
+                                "Scotian Shelf",
+                                "Gulf of St. Lawrence - Eastern Scotian Shelf"
+                              ))) |>
+  # Some nicer names
+  mutate(focalUnit = case_when(
+    focalUnit == "biomass" ~ "Biomass",
+    focalUnit == "percent_cover" ~ "Percent cover",
+    focalUnit == "stipe_density" ~ "Stipe density"
+  ))
