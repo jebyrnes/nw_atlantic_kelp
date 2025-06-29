3f87dad3746386d785c739bb77d2b33825d60c40
Jarrett Byrnes <jarrett.byrnes@umb.edu>
2025-06-29 14:05:38 +0200

    Moving things about.

--- scripts/old/06_env_timeseries_analysis.R
+++ scripts/old/06_env_timeseries_analysis.R
@@ -12,10 +12,34 @@ library(modelbased)
 library(glmmTMB)
 library(broom.mixed)
 library(car)
+library(mregions)
 library(sf)
-library(blme)
 
 theme_set(theme_bw(base_size = 12))
+
+env_dat <- read_csv("data/merged_envt_data_all.csv") |>
+  mutate(id = paste(latitude, longitude) |> as.factor() |> as.numeric() |> as.character(),
+         year_c = year - mean(year),
+         year_z = year_c/sd(year),
+         year_f = as.factor(year))
+
+
+ecoregions_shp <- mr_shp(key = "Ecoregions:ecoregions") |> 
+  st_make_valid() |>
+  dplyr::select(ecoregion, geometry)
+
+
+add_geo_info <- function(dat){
+  dat |>
+    st_as_sf(coords = c("longitude", "latitude"),
+             crs = st_crs(ecoregions_shp),
+             remove = FALSE)|>
+    st_join(ecoregions_shp, 
+            join = st_nearest_feature)
+}
+
+env_dat <- env_dat |>
+  add_geo_info()
 
 get_unique_cells <- function(dat, cell_var){
   dat |>
@@ -38,28 +62,68 @@ ggplot(env_dat |> get_unique_cells(hadsst_seasonal_cell_number),
   guides(color = "none") +
   labs(x = "", y = "HADSST ºC", subtitle = "Summer Average")
 
-hadsst_summer_mod <- glmmTMB(
+ggplot(env_dat|> get_unique_cells(hadsst_seasonal_cell_number),
+       aes(x = year, y = hadsst_spring, 
+           group = id, color = id)) +
+  geom_line(alpha = 0.01) +
+  geom_smooth(method = "lm", fill = NA) +
+  scale_color_viridis_d(option = "F", direction = -1) +
+  guides(color = "none")+
+  labs(x = "", y = "HADSST ºC", subtitle = "Spring Average")
+
+
+hadsst_summer_lm <- glmmTMB(
   hadsst_summer ~ 
     year  + (year|id),
   data = env_dat |> 
     get_unique_cells(hadsst_seasonal_cell_number) # subset so not huge
 )
 
+#had_slopes <- estimate_slopes(hadsst_lm, trend = "year")
+emmeans::emtrends(hadsst_summer_lm,  var = "year") |> 
+  as_tibble() |>
+ggplot(
+       aes(x = year.trend)) +
+  geom_histogram(fill = "pink") +
+  geom_vline(xintercept = 0, lty = 2, color = "red")
+
 # same as initial ggplot
-estimate_relation(hadsst_summer_mod, 
-                  include_random = TRUE,
-                  ci = NA) |>
+estimate_relation(hadsst_summer_lm, ci = NA) |>
   plot(show_data = TRUE,
        line = list(size = 1)) +
   guides(color = "none", fill = "none") +
   scale_color_viridis_d(option = "F", direction = -1) 
-
-hadsst_spring_mod <- glmmTMB(
+  
+hadsst_spring_lm <- lm(
   hadsst_spring ~ 
-    year + (1+year|id),
+    year * id,
   data = env_dat |>
     get_unique_cells(hadsst_seasonal_cell_number) # subset so not huge
 )
+
+# dist of slopes
+emmeans::emtrends(hadsst_spring_lm, ~id, var = "year") |> 
+  as_tibble() |>
+ggplot(
+       aes(x = year.trend)) +
+  geom_histogram(fill = "pink") +
+  geom_vline(xintercept = 0, lty = 2, color = "red")
+
+
+# Smooth to deal with autocorrelation
+  
+hadsst_gam_mod <- mgcv::gam(
+  hadsst_summer ~ 
+    year * id +
+    s(year, by = id), 
+  data = env_dat |>
+    get_unique_cells(hadsst_seasonal_cell_number)
+)
+
+estimate_relation(hadsst_gam_mod, ci = NA) |> 
+  plot(show_data = TRUE) +
+  guides(color = "none", fill = "none") +
+  scale_color_viridis_d(option = "F", direction = -1) 
 
 ##
 # oisst
@@ -86,21 +150,6 @@ ggplot(env_dat|> get_unique_cells(oisst_seasonal_cell_number) |>
   guides(color = "none")+
   labs(x = "", y = "OISST ºC", subtitle = "Spring Average")
 
-
-oisst_summer_mod <- glmmTMB(
-  oisst_summer ~ 
-    year + (1+year|id),
-  data = env_dat |>
-    get_unique_cells(oisst_seasonal_cell_number) # subset so not huge
-)
-
-oisst_spring_mod <- glmmTMB(
-  oisst_spring ~ 
-    year + (1+year|id),
-  data = env_dat |>
-    get_unique_cells(oisst_seasonal_cell_number) # subset so not huge
-)
-
 ##
 # swh
 ##
@@ -126,20 +175,47 @@ ggplot(env_dat|> get_unique_cells(era5_waves_seasonal_cell_number) |>
   labs(x = "", y = "Significant Wave Height (m)", subtitle = "Winter Average")
 
 
-swh_fall_mod <- glmmTMB(
+
+swh_fall_lm <- lm(
   swh_fall ~ 
-    year + (1|id),
+    year * id,
   data = env_dat |>
     get_unique_cells(era5_waves_seasonal_cell_number) # subset so not huge
 )
 
+# Analysis
+Anova(swh_fall_lm)
+tidy(swh_fall_lm)[1:5,]
+emmeans::emtrends(swh_fall_lm, ~1, "year")
+
+# dist of slopes
+emmeans::emtrends(swh_fall_lm, ~id, var = "year") |> 
+  as_tibble() |>
+  ggplot(
+    aes(x = year.trend)) +
+  geom_histogram(fill = "pink") +
+  geom_vline(xintercept = 0, lty = 2, color = "red")
+
 # swh in winter
-swh_winter_mod <- glmmTMB(
+swh_winter_lm <- lm(
   swh_winter ~ 
-    year + (1|id),
+    year * id,
   data = env_dat |>
     get_unique_cells(era5_waves_seasonal_cell_number) # subset so not huge
 )
+
+# Analysis
+Anova(swh_winter_lm)
+tidy(swh_winter_lm)[1:5,]
+emmeans::emtrends(swh_winter_lm, ~1, "year")
+
+# dist of slopes
+emmeans::emtrends(swh_winter_lm, ~id, var = "year") |> 
+  as_tibble() |>
+  ggplot(
+    aes(x = year.trend)) +
+  geom_histogram(fill = "pink") +
+  geom_vline(xintercept = 0, lty = 2, color = "red")
 
 ##
 # no3
@@ -157,14 +233,25 @@ ggplot(env_dat|> get_unique_cells(cmems_seasonal_cell_number) |>
 
 
 
-no3_spring_mod <- glmmTMB(
+no3_spring_lm <- lm(
   no3_spring ~ 
-    year_c + (1 + year_c| id),
-  REML = TRUE,
+    year * id,
   data = env_dat |>
     get_unique_cells(cmems_seasonal_cell_number) # subset so not huge
 )
 
+# Analysis
+Anova(no3_spring_lm)
+emmeans::emtrends(no3_spring_lm, ~1, "year")
+
+# dist of slopes
+emmeans::emtrends(no3_spring_lm, ~id, var = "year") |> 
+  as_tibble() |>
+  ggplot(
+    aes(x = year.trend)) +
+  geom_histogram(fill = "pink", bins = 100) +
+  geom_vline(xintercept = 0, lty = 2, color = "red",
+             size = 0.3) 
 
 ## winter
 
@@ -179,13 +266,25 @@ ggplot(env_dat|> get_unique_cells(cmems_seasonal_cell_number) |>
   labs(x = "", y = "Nitrate", subtitle = "Winter Average")
 
 
-no3_winter_mod <- glmmTMB(
+no3_winter_lm <- lm(
   no3_winter ~ 
-    year_c + (1+year_c|id),
+    year * id,
   data = env_dat |>
     get_unique_cells(cmems_seasonal_cell_number) # subset so not huge
 )
 
+# Analysis
+Anova(no3_winter_lm)
+emmeans::emtrends(no3_winter_lm, ~1, "year")
+
+# dist of slopes
+emmeans::emtrends(no3_winter_lm, ~id, var = "year") |> 
+  as_tibble() |>
+  ggplot(
+    aes(x = year.trend)) +
+  geom_histogram(fill = "pink", bins = 100) +
+  geom_vline(xintercept = 0, lty = 2, color = "red",
+             size = 0.3) 
 
 ##
 # kd490
@@ -202,45 +301,21 @@ ggplot(env_dat|> get_unique_cells(turb_seasonal_cell_number) |>
   labs(x = "", y = "Turbidity (kd490)", subtitle = "Spring Average")
 
 
-kd490_spring_mod <- glmmTMB(
+kd490_spring_lm <- lm(
   kd490_spring ~ 
-    year + (1 |id),
-  REML = TRUE,
+    year * id,
   data = env_dat |>
     get_unique_cells(turb_seasonal_cell_number) # subset so not huge
 )
 
-###
-# Put it all together
-###
-get_year_coef <- function(mod, coef = "year", name = NA){
-  tidy(mod) |>
-    filter(term == coef) |>
-    select(estimate, std.error, p.value) |>
-    mutate(model = name) |>
-    relocate(model, estimate, std.error, p.value) |>
-    rename(
-      ` ` = model,
-      coefficient = estimate,
-      se = std.error,
-      p = p.value
-    )
-}
+# Analysis
+Anova(kd490_spring_lm)
+emmeans::emtrends(kd490_spring_lm, ~1, "year")
 
-envt_tab <- bind_rows(
-  get_year_coef(hadsst_summer_mod, name = "HADSST summer"),
-  get_year_coef(hadsst_spring_mod, name = "HADSST spring"),
-  get_year_coef(oisst_summer_mod, name = "OISST summer"),
-  get_year_coef(oisst_spring_mod, name = "OISST summer"),
-  get_year_coef(swh_fall_mod, name = "SWH fall"),
-  get_year_coef(swh_winter_mod, name = "SWH winter"),
-  get_year_coef(no3_winter_mod, name = "NO3 winter"),
-  get_year_coef(no3_spring_mod, name = "NO3 spring"),
-  get_year_coef(kd490_spring_mod, name = "Turbidity spring")
-)
-
-write_csv(envt_tab, "tables/envt_coefs.csv")
-
-gt::gt(envt_tab) |> 
-  gt::fmt_number(decimals=3) |>
-  gt::gtsave("tables/envt_coefs_gt.docx")
+# dist of slopes
+emmeans::emtrends(kd490_spring_lm, ~id, var = "year") |> 
+  as_tibble() |>
+  ggplot(
+    aes(x = year.trend)) +
+  geom_histogram(fill = "pink") +
+  geom_vline(xintercept = 0, lty = 2, color = "red")
